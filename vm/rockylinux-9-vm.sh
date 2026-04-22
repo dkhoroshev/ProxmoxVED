2ca7e33c29acc913a80f38821e1d8a4dd600c545
Dmitriy Khoroshev <dkhoroshev>
2026-04-22 11:50:38 +0300

    fix qm create

--- vm/rockylinux-9-vm.sh
+++ vm/rockylinux-9-vm.sh
@@ -599,8 +599,8 @@ if [[ "$STORAGE_TYPE" != "nfs" && "$STORAGE_TYPE" != "dir" ]]; then
 fi
 
 msg_info "Creating a Rocky Linux 9 VM"
-qm create "$VMID" -agent 1${MACHINE} -tablet 0 -localtime 1 -bios ovmf${CPU_TYPE} -cores "$CORE_COUNT" -memory "$RAM_SIZE" \
-  -name "$HN" -tags community-script -net0 virtio,bridge="$BRG",macaddr="$MAC""$VLAN""$MTU" -onboot 1 -ostype l26 -scsihw virtio-scsi-pci
+qm create "$VMID" -agent 1 -machine ${MACHINE} -tablet 0 -localtime 1 -bios ovmf ${CPU_TYPE} -cores "$CORE_COUNT" -memory "$RAM_SIZE" \
+  -name "$HN" -net0 virtio,bridge="$BRG",macaddr="$MAC""$VLAN""$MTU" -onboot 1 -ostype l26 -scsihw virtio-scsi-pci
 pvesm alloc "$STORAGE" "$VMID" "$DISK0" 4M 1>&/dev/null
 pvesm alloc "$STORAGE" "$VMID" "$DISK2" 4M 1>&/dev/null
 qm importdisk "$VMID" "${WORK_FILE}" "$STORAGE" ${DISK_IMPORT:-} 1>&/dev/null
